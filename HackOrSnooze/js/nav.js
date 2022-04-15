0d2ab6a505adeba5b527c51bea7579dbb19474d9
maryamsoltani <soltani.maryam@gmail.com>
2022-04-14 22:48:29 -0700

    Update nav.js

--- HackOrSnooze/js/nav.js
+++ HackOrSnooze/js/nav.js
@@ -4,6 +4,7 @@
  * Handling navbar clicks and updating navbar
  */
 
+
 /** Show main list of all stories when click site name */
 
 function navAllStories(evt) {
@@ -14,10 +15,8 @@ function navAllStories(evt) {
 
 $body.on("click", "#nav-all", navAllStories);
 
-/** Show story submit form on clicking story "submit" */
-
 function navSubmitStoryClick(evt) {
-  console.debug("navSubmitStoryClick", evt);
+  console.debug("navSubmitClick", evt);
   hidePageComponents();
   $allStoriesList.show();
   $submitForm.show();
@@ -76,59 +75,3 @@ function updateNavOnLogin() {
   $navLogOut.show();
   $navUserProfile.text(`${currentUser.username}`).show();
 }
-
-
-$navSubmit.on("click", submitStoryClick);
-
-/** Show favorites */
-
-function navFavoritesClick(evt) {
-	console.debug("navFavoritesClick", evt);
-	hidePageComponents();
-	putFavoritesOnPage();
-	$favoritesList.show();
-}
-
-$navFavorites.on("click", navFavoritesClick);
-
-/** Show own stories */
-
-const navMyStoriesClick = (evt) => {
-	console.debug("navMyStoriesClick", evt);
-	hidePageComponents();
-	putUserStoriesOnPage();
-	$ownStoriesList.show();
-};
-
-$navOwnStories.on("click", navMyStoriesClick);
-
-/** When a user first logins in, update the navbar to reflect that. */
-
-function mobileFlexToBlock(tag) {
-	$(tag).removeClass("hidden")
-	// $(tag).show();
-	if (window.matchMedia('(max-width: 767px)').matches) {
-		$(tag).css('display','block')
-	} else if (window.matchMedia('(min-width: 768px)').matches) {
-		$(tag).css('display','flex')
-	}
-}
-
-function updateNavOnLogin() {
-	console.debug("updateNavOnLogin");
-	mobileFlexToBlock(".main-nav-links")
-	$navLogin.hide();
-	$navLogOut.show();
-	$navUserProfile.text(`${currentUser.username}`).show();
-}
-
-/** Show user profile */
-
-const navProfileClick = (evt) => {
-	console.debug("navProfileClick", evt);
-	hidePageComponents();
-	showUserProfile();
-	$userProfileSection.show();
-};
-
-$navUserProfile.on("click", navProfileClick);
